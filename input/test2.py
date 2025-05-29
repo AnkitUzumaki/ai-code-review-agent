d68ee174f2444b8027c23b4b98f80a4d44bc0a5a
AnkitUzumaki <115879470+AnkitUzumaki@users.noreply.github.com>
2025-05-29 13:21:52 +0530

    Add files via upload

---
+++ input/test2.py
@@ -0,0 +1,7 @@
+def calculate_sum(a, b):
+    result = a + b
+    very_long_variable_name_to_trigger_line_length_issue = 42
+    return result
+
+def greet_user(name):
+    print("Hello, " + name + ", welcome!")
